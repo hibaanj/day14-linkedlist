6549bbb3a54acff7ef44c08df010584b2e061f21
hibaanj <anjumhiba15@gmail.com>
2022-08-01 10:54:53 +0530

    UC4-add in middle

---
+++ day14/UC3-linkedlist/Program.cs
@@ -0,0 +1,13 @@
+﻿using UC4_linkedlist;
+
+Console.WriteLine("day14 UC1 - Linkedlist list problem");
+Linkedlist list = new Linkedlist();
+//list.Add(56);
+//list.(30);
+//list.Infront();
+list.Add(70);
+list.Infront(30);
+list.InMiddle(25);
+//list.Add(30);
+list.Infront(56);
+list.Display();
