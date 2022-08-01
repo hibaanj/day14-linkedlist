f8004bf8bd18ace0516a9345a12106099e6b573f
hibaanj <anjumhiba15@gmail.com>
2022-08-01 10:50:03 +0530

    uc2

---
+++ day14/UC2-linkedlist/Linkedlist.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_linkedlist
+{
+    internal class Linkedlist
+    {
+        public Node head;
+        //method add
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"{node.data} addaed to linked list");
+        }
+        //to add value in the front instead of last
+        public void Infront(int data) // add front to the list
+        {
+            Node node = new Node(data);
+            if (this.head == null) 
+                this.head = null;
+            else
+            {
+                node.next = this.head; //shifting the address from head to new nodes next
+                this.head = node;
+            }
+            Console.WriteLine($" {node.data} is added to the front");
+        }
+
+        //todisplay the value
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("linked list is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+
+    }
+}
---
+++ day14/UC2-linkedlist/Program.cs
@@ -0,0 +1,12 @@
+﻿using UC2_linkedlist;
+
+Console.WriteLine("day14 UC1 - Linkedlist list problem");
+Linkedlist list = new Linkedlist();
+//list.Add(56);
+//list.(30);
+//list.Infront();
+list.Add(70);
+list.Infront(30);
+//list.Add(30);
+list.Infront(56);
+list.Display();
