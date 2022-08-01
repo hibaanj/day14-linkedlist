c08ad4d10e562e9c39bba2bf8e26494fc28c0859
hibaanj <anjumhiba15@gmail.com>
2022-08-01 10:46:43 +0530

    uc1 of day 14 linked list

---
+++ day14/day14/Linkedlist.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day14
+{
+    internal class Linkedlist
+    {
+        public Node head;
+        //method add
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+               Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"{node.data} addaed to linked list");
+        }
+        //todisplay the value
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("linked list is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+    }
+}
---
+++ day14/day14/Program.cs
@@ -0,0 +1,8 @@
+﻿using day14;
+
+Console.WriteLine("day14 UC1 - Linkedlist list problem");
+Linkedlist list = new Linkedlist();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+list.Display();
