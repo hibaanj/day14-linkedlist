0b97646730b714992edc9245a242ab36faa23277
hibaanj <anjumhiba15@gmail.com>
2022-08-02 17:23:36 +0530

    removing last node

---
+++ UC6-removelast/Program.cs
@@ -0,0 +1,17 @@
+﻿using UC6_removelast;
+
+Console.WriteLine("day14 UC5 - Linkedlist list problem");
+Linkedlist list = new Linkedlist();
+//list.Add(56);
+//list.(30); 
+//list.Infront();
+list.Removelast();
+list.RemoveFirst();
+list.Add(70);
+list.Infront(30);
+list.InMiddle(25);
+list.Add(25);
+//list.Add(30);
+
+list.Infront(56);
+list.Display();
